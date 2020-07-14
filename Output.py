c32a1aa2668ef7db15db09359186012d1cf21b8b
Manikandan P <muthumanip21@gmail.com>
2020-07-14 17:45:01 +0530

    Update Output.py

--- Output.py
+++ Output.py
@@ -3,10 +3,13 @@ import json
 import pandas as pd
 import sys, getopt, pprint
 from pymongo import MongoClient
-
+# Get the csv file
 csvfile = open('myfile.csv','r')
+# Read the file
 reader = csv.DictReader(csvfile)
+#MongoDb connection
 mongo_client = MongoClient()
+#Insert collection in table
 header = ['Transaction_date', 'Product', 'Price', 'Payment_Type', 'Name', 'City', 'State', 'Country', 'Account_created'
     , 'last_login', 'Latutude', 'Longtitude']
 for i in reader:
