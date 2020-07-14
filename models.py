51a485ef914fee52b6e468a1dc69eed37c4d1be3
Manikandan P <muthumanip21@gmail.com>
2020-07-14 15:34:34 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,23 @@
+from django.db import models
+import pymongo
+
+class my_database(models.Model):
+
+    # CONNECT TO DATABASE
+    connection = pymongo.MongoClient('localhost', 27017)
+
+    # CREATE DATABASE
+    database = connection['mydatabase']
+
+    # CREATE COLLECTION
+    collection = database['mycollection']
+
+    # Insert data
+    collection.insert_data = {'Transaction_date', 'Product', ' Price', 'Payment_Type', 'Name', 'City', 'State',
+                              'Country', 'Account_created'
+        , 'last_login', 'Latitude', 'Longtitude'}
+
+    def get_multiple_data():
+        data = collection.find()
+        return list(data)
+
