182b2d1d3715bec8faf1abfc352e553a705bf827
Manikandan P <muthumanip21@gmail.com>
2020-07-14 15:49:13 +0530

    Add files via upload

---
+++ Output.py
@@ -0,0 +1,16 @@
+import csv
+import json
+import pandas as pd
+import sys, getopt, pprint
+from pymongo import MongoClient
+
+csvfile = open('r')
+reader = csv.DictReader(csvfile)
+mongo_client = MongoClient()
+header = ['Transaction_date', 'Product', 'Price', 'Payment_Type', 'Name', 'City', 'State', 'Country', 'Account_created'
+    , 'last_login', 'Latutude', 'Longtitude']
+for i in reader:
+    s = {}
+    for j in header:
+        s[j] = i[j]
+    db.segment.insert(s)
